eff54562624890e7fb74a07dfe94acbb919190ab
letschanator <austin@letsch.org>
2022-12-01 17:20:18 -0600

    Finished Austin Letsch page except for picture.

--- src/pages/AustinLetsch.jsx
+++ src/pages/AustinLetsch.jsx
@@ -1,5 +1,27 @@
+import Comments from '../components/UtteranceComments'
+
 function AustinLetsch(){
-    return <h1>Austin Letsch</h1>
+    return (<><div style={{ textAlign: "center", color: 'black', backgroundColor: 'gold' }}>
+        <p></p>
+        <p></p>
+        <h1>Austin Letsch</h1>
+        <p></p>
+        <h2>About Me</h2>
+        <h3>    Austin currently attends the University of Iowa and is in his 4th year </h3>
+        <h3>as a Computer Science and Engineering major with a machine learning efa.</h3>
+        <h3>I was born in Brooklyn Center (a subarb of Minneapolis) Minnesota.  I moved</h3>
+        <h3>to West Des Moines when I was 10 and have stayed in Iowa since.Outside of </h3>
+        <h3>School I like to play tabletop games, espsially card games like Magic: the Gathering.</h3>
+        <h3>I have a Younger brother named Ethan and 2 dogs named Cosmo and Oreo.</h3>
+
+        <h2>Contact Me</h2>
+        <h3>phone: 515-777-0600</h3>
+        <h3>email: aletsch@uiowa.edu</h3>
+        <h3>Github: <a href="http://www.github.com/letschanator">letschanator</a> </h3>
+    </div><div className='Comments'>
+            <Comments />
+        </div></>
+            )
 }
 
 export default AustinLetsch;
--- src/pages/LukeMartin.jsx
+++ src/pages/LukeMartin.jsx
@@ -1,22 +1,23 @@
 function LukeMartin(){
-    <div style={{textAlign:"center", color: 'black', backgroundColor: 'gold'}}>
-        <p></p>
-        <p></p>
-        <h1>Luke Martin</h1>
-        <p></p>
-        <h2>About Me</h2>
-        <h3>    Luke currently attends the University of Iowa and is in his 4th year </h3>
-        <h3>as a Computer Science and Engineering major. He is originally from Sioux </h3>
-        <h3>City, Iowa. Luke's hobby's outside of school are watching football, going</h3>
-        <h3>to concerts, and eating large variety's of different foods. He is the    </h3>
-        <h3>middle child of a family of five with an older sister and younger brother. </h3>
-        <h3>Luke hopes to become a software developer after he graduates from Iowa. </h3>
+    return(<div style={{textAlign:"center", color: 'black', backgroundColor: 'gold'}}>
+            <p></p>
+            <p></p>
+            <h1>Luke Martin</h1>
+            <p></p>
+            <h2>About Me</h2>
+            <h3>    Luke currently attends the University of Iowa and is in his 4th year </h3>
+            <h3>as a Computer Science and Engineering major. He is originally from Sioux </h3>
+            <h3>City, Iowa. Luke's hobby's outside of school are watching football, going</h3>
+            <h3>to concerts, and eating large variety's of different foods. He is the    </h3>
+            <h3>middle child of a family of five with an older sister and younger brother. </h3>
+            <h3>Luke hopes to become a software developer after he graduates from Iowa. </h3>
         
-        <h2>Contact Me</h2>
-        <h3>phone: 712-281-4090</h3>
-        <h3>email: lmartin9@uiowa.edu</h3>
-        <h3>LinkedIn: </h3>
-    </div>
+            <h2>Contact Me</h2>
+            <h3>phone: 712-281-4090</h3>
+            <h3>email: lmartin9@uiowa.edu</h3>
+            <h3>LinkedIn: </h3>
+            </div>
+    )
 }
 
 export default LukeMartin;
