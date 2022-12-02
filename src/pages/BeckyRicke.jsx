400a76f5697832faa321aef0574f540a1fda51dc
Luke Martin <106223617+lukemartin3@users.noreply.github.com>
2022-12-01 18:04:31 -0600

    Update BeckyRicke.jsx

--- src/pages/BeckyRicke.jsx
+++ src/pages/BeckyRicke.jsx
@@ -1,5 +1,28 @@
+import Comments from '../components/UtteranceComments'
+
 function BeckyRicke(){
-    return <h1>Becky Ricke</h1>
-}
+    return(
+    <div style={{textAlign:"center", color: 'black', backgroundColor: 'gold'}}>
+            <h1>Becky Ricke</h1>
+            <p></p>
+            <h2>About Me</h2>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+
+            <h2>Contact Me</h2>
+            <h4>phone: </h4>
+            <h4>email: </h4>
+            <h4>LinkedIn: </h4> <a href="https://www.linkedin.com/in/rebecca-ricke/">Becky Ricke</a>
+
+
+            <div className='Comments'>
+                <Comments/>
+            </div>
+        </div>
+);}
 
 export default BeckyRicke;
