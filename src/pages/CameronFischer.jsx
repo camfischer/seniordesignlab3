527426ac290dca934a831f88a6928b40ea6c29ba
letschanator <austin@letsch.org>
2022-12-02 11:16:16 -0600

    fixed formating on camron page

--- src/pages/CameronFischer.jsx
+++ src/pages/CameronFischer.jsx
@@ -3,12 +3,14 @@ import CameronPicture from './Cam.png'
 function CameronFischer(){
     return (
         <div style={{textAlign:"center", color: 'black', backgroundColor: 'gold'}}>
-            <img style={{width: 231.5, height: 400, border: '1px solid black'}} src={CameronPicture}/>a
+            <img style={{width: 231.5, height: 400, border: '1px solid black'}} src={CameronPicture}/>
             <h1>Cameron Fischer</h1>
             <p></p>
             <h2>About Me:</h2>
-            <h3>I'm from Davenport, Iowa. Currently majoring in Computer Science and Engineering with a focus in Software Engineering. I plan to work in the .NET environment with a focus on backend
-                development. In my freetime I like to play soccer, video games, and go to the gym. 
+            <h3>I'm from Davenport, Iowa. Currently majoring in Computer Science and Engineering</h3>
+            <h3> with a focus in Software Engineering. I plan to work in the .NET environment</h3> 
+            <h3>with a focus on backend development. In my freetime I like to play soccer, </h3>
+            <h3> video games, and go to the gym. 
             </h3>
             <h2>Contact Me:</h2>
             <h4>email: cameron-fischer@uiowa.edu </h4>
