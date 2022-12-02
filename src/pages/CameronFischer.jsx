7cf49c4fa07c03717952fcc28a6881d8333d6f15
Luke Martin <106223617+lukemartin3@users.noreply.github.com>
2022-12-01 18:04:08 -0600

    Update CameronFischer.jsx

--- src/pages/CameronFischer.jsx
+++ src/pages/CameronFischer.jsx
@@ -1,5 +1,29 @@
+import Comments from '../components/UtteranceComments'
+
 function CameronFischer(){
-    return <h1>Cameron Fischer</h1>
+    return (
+        <div style={{textAlign:"center", color: 'black', backgroundColor: 'gold'}}>
+            <h1>Cameron Fischer</h1>
+            <p></p>
+            <h2>About Me</h2>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+            <h3></h3>
+
+            <h2>Contact Me</h2>
+            <h4>phone: </h4>
+            <h4>email: </h4>
+            <h4>LinkedIn: </h4> <a href="">Cameron Fischer</a>
+
+
+            <div className='Comments'>
+                <Comments/>
+            </div>
+        </div>
+        );
 }
 
 export default CameronFischer;
