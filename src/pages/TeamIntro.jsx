7474d994a9ab2f38ea83eed11d1897d67cb25432
letschanator <austin@letsch.org>
2022-12-02 11:05:48 -0600

    got rid of fun facts

--- src/pages/TeamIntro.jsx
+++ src/pages/TeamIntro.jsx
@@ -9,25 +9,21 @@ function TeamIntro(){
             <h3 style={{color:"white"}}>Year: Senior </h3>
             <h3 style={{color:"white"}}>Major: Computer Science and Engineering</h3>
             <h3 style={{color:"white"}}>Hometown: Davenport, IA</h3> 
-            <h3 style={{color:"white"}}>Fun fact: </h3> 
             
             <h2>Luke Martin</h2>
             <h3 style={{color:"white"}}>Year: Senior </h3>
             <h3 style={{color:"white"}}>Major: Computer Science and Engineering</h3>
             <h3 style={{color:"white"}}>Hometown: Sioux City, IA </h3> 
-            <h3 style={{color:"white"}}>Fun fact: Born in South Korea</h3> 
     
             <h2>Austin Letsch</h2>
             <h3 style={{color:"white"}}>Year: Senior </h3>
             <h3 style={{color:"white"}}>Major: Computer Science and Engineering</h3>
             <h3 style={{color:"white"}}>Hometown: Des Moines, IA</h3> 
-            <h3 style={{color:"white"}}>Fun fact: </h3> 
     
             <h2>Becky Ricke</h2>
             <h3 style={{color:"white"}}>Year: Senior </h3>
             <h3 style={{color:"white"}}>Major: Computer Science and Engineering</h3>
             <h3 style={{color:"white"}}>Hometown: Royal, IA </h3> 
-            <h3 style={{color:"white"}}>Fun fact: </h3> 
     
         </div>
         );
